73dba23b3eeb925c6a80444b5d46d6f06c627b01
MiaAvishaMMontinola <miaavisha.montinola@smc.pshs.edu.ph>
2026-01-12 22:34:52 +0800

    Create Montinola_Q3_FA02.py

---
+++ Montinola_Q3_FA02.py
@@ -0,0 +1,22 @@
+Scebario: Weekly test scores of 3 subjects for 5 students
+
+
+scores = [
+   [89,97,83],
+   [76,84,89],
+   [97,94,92],
+   [65,73,78],
+   [83,89,94]
+]
+
+subjects = ["Math", "Science", "English"]
+
+print("Student", subjects)
+
+for i in range(len(scores)):
+    print("Student", i+1, scores[i])
+
+Reflection:
+ I picked this datset because it is simple to execute but still makes good practice. 
+2D arrays allow us to look at datasets us table for easy viewing and lets us access the data easily
+
